19de0838fa13113df24365df59b54e7c717c09ed
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-05-09 12:29:16 -0300

    Add files via upload

---
+++ Exer06.sql
@@ -0,0 +1,62 @@
+CREATE DATABASE Trabalho;
+USE Trabalho;
+CREATE TABLE Cidade(
+	idcidade INT NOT NULL auto_increment,
+    descricao VARCHAR(50),
+    primary key(idcidade)
+) ENGINE=INNODB;
+CREATE TABLE Departamento (
+	iddepartamento INT NOT NULL auto_increment,
+    descricao VARCHAR(45),
+    telefone VARCHAR(15),
+    cidadeid int,
+    primary key(iddepartamento),
+    foreign key(cidadeid) references Cidade(idcidade)
+)ENGINE=INNODB;
+CREATE TABLE Funcionario(
+	idfuncionario INT NOT NULL auto_increment,
+    Nome VARCHAR(80),
+    Nascimento DATE,
+    Sexo CHAR(1),
+    Admissao datetime,
+    Salario DECIMAL(8,2),
+    departamentoid INT,
+    primary key(idfuncionario),
+    foreign key(departamentoid) references Departamento (iddepartamento)
+)ENGINE=INNODB;
+
+INSERT INTO Cidade values (1 ,'Marilia');
+INSERT INTO Cidade values (2 ,'Vera Cruz');
+INSERT INTO Cidade values (3 ,'Pompéia');
+INSERT INTO Cidade values (4 ,'Garça');
+
+INSERT INTO Departamento values (10 ,'Diretoria','(14)3433 1515', '1');
+INSERT INTO Departamento values (20 ,'RH','(14)3433 1516', '1');
+INSERT INTO Departamento values (30 ,'TI','(14)3492 1718', '2');
+INSERT INTO Departamento values (40 ,'TI','(14)3471 1821', '4');
+INSERT INTO Departamento values (50 ,'Vendas','(14)3471 1822', '4');
+
+INSERT INTO Funcionario values (100 ,'Ana','1980-05-15', 'F', '2010-03-01', 7500.00, 10);
+INSERT INTO Funcionario values (200 ,'Maria','1978-12-13', 'F', '2012-09-15', 3938.12, 10);
+INSERT INTO Funcionario values (300 ,'Jose','1997-03-08', 'M', '2009-03-05', 5200.00, 20);
+INSERT INTO Funcionario values (400 ,'Aparecido','1979-06-25', 'M', '2011-04-10', 7385.59, 30);
+INSERT INTO Funcionario values (500 ,'Marcia','1985-02-03', 'F', '2010-03-01', 3500.00, 40);
+INSERT INTO Funcionario values (600 ,'Orlando','1983-12-25', 'M', '2012-06-10', 2754.15, 20);
+
+select * from Cidade; 
+
+select iddepartamento, descricao, cidadeid from Departamento where descricao = 'TI';
+
+select Nome, Sexo from Funcionario where departamentoid >=20;
+
+select Nome, Admissao from Funcionario where Admissao BETWEEN '2010-01-01' AND '2010-12-31';
+
+select * from Funcionario where Salario > 3500.00 and Sexo = 'F';
+
+update Funcionario SET Salario = Salario * 1.10 where departamentoid = 20;
+
+update Funcionario SET Admissao = (STR_TO_DATE(DATE_FORMAT(NOW(), '%d/%m/%y'), '%d/%m/%y')) where idfuncionario = 500;
+
+-- update Funcionario SET Admissao = NOW() WHERE idfuncionario = 500;
+
+Delete from Cidade where descricao = 'Pompéia'; 
---
+++ ModeloAulaDQL.sql
@@ -0,0 +1,184 @@
+
+-- -----------------------------------------------------
+-- Schema auladql
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS auladql DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE auladql ;
+
+-- -----------------------------------------------------
+-- Tabela estado
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS estado (
+  uf CHAR(2) NOT NULL,
+  nome VARCHAR(50) NOT NULL,
+  PRIMARY KEY (uf)
+) ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Tabela cidade
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS cidade (
+  idcidade INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(60) NOT NULL,
+  uf CHAR(2) NOT NULL,
+  PRIMARY KEY (idcidade),
+  CONSTRAINT fk_cidade_estado
+    FOREIGN KEY (uf)
+    REFERENCES estado (uf)
+) ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Tabela bairro
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS bairro (
+  idbairro INT NOT NULL,
+  descricao VARCHAR(45) NOT NULL,
+  idcidade INT UNSIGNED NOT NULL,
+  PRIMARY KEY (idbairro),
+  CONSTRAINT fk_bairro_cidade
+    FOREIGN KEY (idcidade)
+    REFERENCES cidade(idcidade)
+) ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Tabela ceps
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS ceps (
+  cep INT UNSIGNED NOT NULL,
+  logradouro VARCHAR(100) NOT NULL,
+  auxiliar VARCHAR(45) NULL,
+  idbairro INT NOT NULL,
+  PRIMARY KEY (cep),
+  CONSTRAINT fk_ceps_bairro
+    FOREIGN KEY (idbairro)
+    REFERENCES bairro (idbairro)
+) ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Tabela cliente
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS cliente (
+  idcliente INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(80) NOT NULL,
+  cadastro DATE NOT NULL,
+  numero VARCHAR(8) NOT NULL,
+  complemento VARCHAR(45) NULL,
+  cep INT UNSIGNED NOT NULL,
+  PRIMARY KEY (idcliente),
+  CONSTRAINT fk_cliente_ceps
+    FOREIGN KEY (cep)
+    REFERENCES ceps (cep)
+) ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Tabela produto
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS produto (
+  idproduto INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  descricao VARCHAR(50) NOT NULL,
+  valor_unitario DECIMAL(8,2) NOT NULL,
+  PRIMARY KEY (idproduto)
+) ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Tabela nfe
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS nfe (
+  numero INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  data DATE NOT NULL,
+  idcliente INT UNSIGNED NOT NULL,
+  PRIMARY KEY (numero),
+  CONSTRAINT fk_nfe_cliente
+    FOREIGN KEY (idcliente)
+    REFERENCES cliente (idcliente)
+) ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Tabela item_nfe
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS item_nfe (
+  numero INT UNSIGNED NOT NULL,
+  idproduto INT UNSIGNED NOT NULL,
+  quantidade DECIMAL(9,4) NOT NULL DEFAULT 1,
+  PRIMARY KEY (numero, idproduto),
+  CONSTRAINT fk_item_nfe_nfe
+    FOREIGN KEY (numero)
+    REFERENCES nfe (numero),
+  CONSTRAINT fk_nfe_has_produto_produto
+    FOREIGN KEY (idproduto)
+    REFERENCES produto (idproduto)
+) ENGINE = InnoDB;
+
+INSERT INTO estado (uf, nome) VALUES ('SP', 'São Paulo');
+INSERT INTO estado (uf, nome) VALUES ('RJ', 'Rio de Janeiro');
+INSERT INTO estado (uf, nome) VALUES ('PR', 'Paraná');
+INSERT INTO estado (uf, nome) VALUES ('MG', 'Minas Gerais');
+
+SELECT * FROM estado;
+
+INSERT INTO cidade (nome,uf) VALUES ('Marília', 'SP');
+INSERT INTO cidade (nome,uf) VALUES ('Vera Cruz', 'SP');
+INSERT INTO cidade (nome,uf) VALUES ('Garça', 'SP');
+INSERT INTO cidade (nome,uf) VALUES ('Pompéia', 'SP');
+INSERT INTO cidade (nome,uf) VALUES ('Assis', 'SP');
+
+SELECT * FROM cidade;
+
+INSERT INTO bairro (idbairro,descricao,idcidade) VALUES (100, 'Centro', 1);
+INSERT INTO bairro (idbairro,descricao,idcidade) VALUES (200, 'Alto Cafezal', 1);
+INSERT INTO bairro (idbairro,descricao,idcidade) VALUES (300, 'Campus Universitário', 1);
+INSERT INTO bairro (idbairro,descricao,idcidade) VALUES (400, 'Maria Isabel', 1);
+INSERT INTO bairro (idbairro,descricao,idcidade) VALUES (500, 'Centro', 2);
+INSERT INTO bairro (idbairro,descricao,idcidade) VALUES (600, 'Aeroporto', 2);
+INSERT INTO bairro (idbairro,descricao,idcidade) VALUES (700, 'Centro', 3);
+INSERT INTO bairro (idbairro,descricao,idcidade) VALUES (800, 'Distrito Industrial', 3);
+INSERT INTO bairro (idbairro,descricao,idcidade) VALUES (900, 'Centro', 4);
+INSERT INTO bairro (idbairro,descricao,idcidade) VALUES (950, 'CECAP', 5);
+
+SELECT * FROM bairro;
+
+INSERT INTO ceps (cep,logradouro,auxiliar,idbairro) VALUES (17509210, 'Aveninda Ipiranga',NULL,100);
+INSERT INTO ceps (cep,logradouro,auxiliar,idbairro) VALUES (17502053, 'Rua Coroados','até 390/391',200);
+INSERT INTO ceps (cep,logradouro,auxiliar,idbairro) VALUES (17504060, 'Rua Coroados','de 392/393 ao fim',200);
+
+SELECT * FROM ceps;
+
+INSERT INTO cliente (nome,cadastro,numero,complemento,cep) VALUES ('Joca',CURDATE(),'48',NULL,17509210);
+INSERT INTO cliente (nome,cadastro,numero,complemento,cep) VALUES ('Marta',CURDATE(),'282',NULL,17502053);
+INSERT INTO cliente (nome,cadastro,numero,complemento,cep) VALUES ('Bia',CURDATE(),'115','Apto 25',17502053);
+INSERT INTO cliente (nome,cadastro,numero,complemento,cep) VALUES ('Wily',CURDATE(),'1580','Fundos',17504060);
+
+SELECT * FROM cliente;
+
+INSERT INTO produto (descricao,valor_unitario) VALUES ('Lápis Nº 2',5.8);
+INSERT INTO produto (descricao,valor_unitario) VALUES ('Caneta esferográfica Azul',1.2);
+INSERT INTO produto (descricao,valor_unitario) VALUES ('Caneta esferográfica Preta',1.2);
+INSERT INTO produto (descricao,valor_unitario) VALUES ('Caneta esferográfica Vermelha',0.98);
+INSERT INTO produto (descricao,valor_unitario) VALUES ('Caderno com 200 folhas',38.59);
+
+SELECT * FROM produto;
+
+INSERT INTO nfe (data,idcliente) VALUES (CURDATE()-2,1);
+INSERT INTO nfe (data,idcliente) VALUES (CURDATE()-1,1);
+INSERT INTO nfe (data,idcliente) VALUES (CURDATE(),2);
+INSERT INTO nfe (data,idcliente) VALUES (CURDATE(),3);
+
+SELECT * FROM nfe;
+
+INSERT INTO item_nfe (numero,idproduto,quantidade) VALUES(1,1,10);
+INSERT INTO item_nfe (numero,idproduto,quantidade) VALUES(1,2,3);
+INSERT INTO item_nfe (numero,idproduto,quantidade) VALUES(1,5,1);
+INSERT INTO item_nfe (numero,idproduto,quantidade) VALUES(2,4,5);
+INSERT INTO item_nfe (numero,idproduto,quantidade) VALUES(3,1,10);
+INSERT INTO item_nfe (numero,idproduto,quantidade) VALUES(4,5,3);
+INSERT INTO item_nfe (numero,idproduto,quantidade) VALUES(4,4,7);
+INSERT INTO item_nfe (numero,idproduto,quantidade) VALUES(4,1,100);
+
+SELECT * FROM item_nfe;
